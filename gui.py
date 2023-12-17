e239fe4250783566534c07a7f10660645a287989
natalia <nat.adamczyk.365@gmail.com>
2023-12-17 20:56:34 +0100

    Navigation Window

---
+++ gui.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+class Gui:
+    functionalities = sorted(
+        ['Create Visual', 'Create Ranking', 'Check Correlation', 'Compare Areas', 'Download Dataset'])
+    def start_program(self):
+        window = tk.Tk()
+        # Displaying window at the center of a screen
+        height = 225
+        width = 450
+        window.geometry(f"{width}x{height}")
+        center_x = int((window.winfo_screenwidth() - width) / 2)
+        center_y = int((window.winfo_screenheight() - height) / 2)
+        window.geometry(f"+{center_x}+{center_y}")
+
+        # Adding buttons for every available function
+        for function in self.functionalities:
+            button = tk.Button(window, text=function, command=lambda option=function: self.button_click(option, window))
+            button.pack(pady=5)
+        window.mainloop()
+
+
+if __name__ == "__main__":
+    try:
+        gui = Gui()
+        gui.start_program()
+    except Exception as err:
+        print(type(err), err)
