66f83d710b6e65c22bc68bf96c48f6a3928bb3e0
natalia <nat.adamczyk.365@gmail.com>
2024-01-03 19:03:28 +0100

    Changing way of downloading data

--- visuals.py
+++ visuals.py
@@ -11,7 +11,7 @@ def create_scatter_plot(name_1 = 'nazwa_1', name_2 = 'nazwa_2', id_1='747060',id
     scatter_plot = sns.scatterplot(x=df[name_1], y=df[name_2], hue='Location', data=df, palette='viridis', legend='full', s=100)
     scatter_plot.legend(loc='upper left', bbox_to_anchor=(1, 1), borderaxespad=0.5)
 
-    # Dodaj tytuł i oznaczenia osi
+    # Dodanie tytuł i oznaczenia osi
     plt.title('Wykres punktowy')
     plt.xlabel(name_1.capitalize(), wrap=True)
     plt.ylabel(name_2.capitalize(), wrap=True)
